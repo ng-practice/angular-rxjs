90008d0c5bcddb13b99e73529076fcfaacafa9d8
Gregor Woiwode <gregor.woiwode@co-IT.eu>
2024-05-05 00:07:50 +0200

    playground: task 1

--- apps/rxjs-playground/src/main.ts
+++ apps/rxjs-playground/src/main.ts
@@ -1,1 +1,15 @@
-console.log('Hello World');
+import { Observable } from 'rxjs';
+
+const stream$ = new Observable((observer) => {
+  observer.next(1);
+  observer.next(2);
+  observer.next(3);
+  observer.next(4);
+  observer.next(5);
+  observer.complete();
+});
+
+stream$.subscribe({
+  next: (value) => console.log(value),
+  complete: () => console.log('Fertig! 😎'),
+});
